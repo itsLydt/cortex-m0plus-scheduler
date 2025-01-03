7f18cbbfce73d5fd8f90afd252535a8a8b2d8b15
itsLydt <lklaus.dev@gmail.com>
2025-01-03 12:42:19 -0600

    added data structure to represent a task

---
+++ task.h
@@ -0,0 +1,26 @@
+/*
+ * task.h
+ *
+ * Created: 1/3/2025 12:35:21 PM
+ *  Author: itsLydt
+ */ 
+
+
+#ifndef TASK_H_
+#define TASK_H_
+
+enum TaskState {
+	READY,
+	BLOCKED
+};
+
+
+struct Task_t {
+	uint32_t stack_addr;		// stack address for this task
+	uint32_t resumption_tick;	// tick at which this task can be resumed 	
+	uint8_t current_state;		// the current state of the task, which is a TaskState 
+	void (*task_handler)(void);	// function pointer for the code to be run by this task
+};
+
+
+#endif /* TASK_H_ */
