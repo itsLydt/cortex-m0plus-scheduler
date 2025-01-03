ef34edab6d868bc7ca38428123f4e3085052840f
itsLydt <lklaus.dev@gmail.com>
2025-01-03 14:10:44 -0600

    added comments to indicate basic program flow

--- main.c
+++ main.c
@@ -16,11 +16,22 @@
 
 int main(void)
 {
+	/* initialize GPIO */
 	uint32_t pin_mask = (1 << LED1) | (1 << LED2) | (1 << LED3);
 	GPIO_SetPortDirection(LED_PORT, pin_mask, GPIO_OUT);			// set LED pins as outputs
 	GPIO_WritePort(LED_PORT, pin_mask, 1);							//turn off all LEDs	
 	
-    while (1) 
-    {
-    }
+	
+	/* Initialize stack space for the scheduler */
+	
+	/* Initialize stack, control block info for each task */
+	
+	/* Switch the scheduler over to PSP (program stack pointer) */
+	
+	/* Enable the systick timer, which effectively starts the scheduler */
+	
+	/* Kick off the first task */
+	
+	/* This will not execute, as the scheduler will execute the idle_handler function if there are no ready tasks */
+    while (1);
 }
