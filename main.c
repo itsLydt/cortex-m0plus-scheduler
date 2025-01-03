7cf9affed52281f778c4a96c66bfe43466521b10
itsLydt <lklaus.dev@gmail.com>
2025-01-03 12:24:49 -0600

    Skeleton project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * RoundRobin.c
+ *
+ * Created: 1/3/2025 12:22:26 PM
+ * Author : itsLydt
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
