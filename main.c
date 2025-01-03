6498da2ce260126f7f52bfe4e9a784addbe0f716
itsLydt <lklaus.dev@gmail.com>
2025-01-03 13:26:46 -0600

    include gpio driver code to, shockingly, blink some LEDs

--- main.c
+++ main.c
@@ -7,11 +7,19 @@
 
 
 #include "sam.h"
+#include "ATSAMD21/drivers/gpio/gpio.h"
 
+#define LED_PORT GPIOB
+#define LED1 4
+#define LED2 5
+#define LED3 6
 
 int main(void)
 {
-    /* Replace with your application code */
+	uint32_t pin_mask = (1 << LED1) | (1 << LED2) | (1 << LED3);
+	GPIO_SetPortDirection(LED_PORT, pin_mask, GPIO_OUT);			// set LED pins as outputs
+	GPIO_WritePort(LED_PORT, pin_mask, 1);							//turn off all LEDs	
+	
     while (1) 
     {
     }
