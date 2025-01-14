03d954b992c64ee971b010332d488f07afe47f52
itsLydt <lklaus.dev@gmail.com>
2025-01-13 18:03:56 -0600

    fixed issue where idle task was not accounted for when calculating max tasks. Added definitions for various address in stack space

--- stack.h
+++ stack.h
@@ -9,9 +9,13 @@
 #ifndef STACK_H_
 #define STACK_H_
 
+#include "sam.h"
+
 /* the Cortex-M0+ uses a full descending stack, meaning that when a new item is to be placed on the stack, 
 	the stack pointer is decremented and the item is written to the new memory location. 
-	Ergo, the stack pointer points to the last item added to the stack.
+	Ergo, the stack pointer points to the last item added to the stack and the "top" of the stack moves 
+	closer to the start address when items are added. When items are removed, the stack pointer is incremented
+	and thus the "bottom" of the stack is closer to the end address.
 */ 
 
 #define SRAM_START HMCRAMC0_ADDR	/* starting address of SRAM in memory map */
@@ -21,5 +25,15 @@
 #define TASK_STACK_LIMIT 1024U				// 1KB
 #define SCHED_STACK_LIMIT TASK_STACK_LIMIT	// give scheduler same stack capacity as task, mainly out of convenience
 
-#define MAX_TASKS ((HMCRAMC0_SIZE / TASK_STACK_LIMIT) - 1)	// number of tasks capable of fitting in SRAM along with the scheduler
+#define STACK_START (SRAM_END - (NUM_TASKS*TASK_STACK_LIMIT + SCHED_STACK_LIMIT))	// starting address of entire stack (all tasks + scheduler) in SRAM
+
+#define SCHED_STACK_END		SRAM_END												// maximum address of scheduler stack space, same as end of SRAM
+#define SCHED_STACK_START	(SCHED_STACK_END - SCHED_STACK_LIMIT)					// minimum address of scheduler stack space
+
+#define IDLE_STACK_END		SCHED_STACK_START										// maximum address of idle task stack space, same as min address of scheduler stack
+#define IDLE_STACK_START	(IDLE_STACK_END - TASK_STACK_LIMIT)						// minimum address of idle task stack space
+
+#define USER_STACK_END		IDLE_STACK_START										// maximum address of user task stack space 
+
+#define MAX_TASKS ((HMCRAMC0_SIZE / TASK_STACK_LIMIT) - 2)	// number of tasks capable of fitting in SRAM along with the scheduler and the idle task
 #endif /* STACK_H_ */
